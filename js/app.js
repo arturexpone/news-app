d72f8b5685defb2403b478c1443ba40c10626638
arturexpone <arturexpone@yandex.ru>
2020-12-30 09:16:59 +0300

    fix: change service obj. add proxy(cors)

--- js/app.js
+++ js/app.js
@@ -3,11 +3,35 @@ document.addEventListener('DOMContentLoaded',  e => {
     M.AutoInit();
 });
 
+class Service {
+    proxyURL = 'https://cors-anywhere.herokuapp.com/';
+
+    constructor(key, url) {
+        this.apiKey = key;
+        this.apiUrl = url;
+    }
+
+    topHeadlines(country = 'ru', callback) {
+        const result = http.get(
+            `${this.proxyURL + this.apiUrl}/top-headlines?country=${country}&apiKey=${this.apiKey}`
+        );
+        result.then(callback);
+    }
+
+    everything(query = '', callback) {
+
+    }
+}
+
 const http = {
     get: async (url, query) => {
         let result;
         try {
-            const response = await fetch(`${url + (query ? `?${query}`: '')}`);
+            const response = await fetch(`${url + (query ? `?${query}`: '')}`, {
+                method: 'GET',
+                'Access-Control-Request-Method': 'GET',
+                'Access-Control-Request-Headers': 'Content-Type, x-requested-with',
+            });
             if (response.ok) {
                 result = await response.json();
             } else {
@@ -41,20 +65,9 @@ const http = {
     }
 }
 
-const newsService = (function () {
-    const apikey = '85540e429d6746bcbafad77a05c7adff';
-    const apiUrl = 'http://newsapi.org/v2';
+const newsService = new Service('85540e429d6746bcbafad77a05c7adff', 'https://newsapi.org/v2');
 
-    return {
-        topHeadlines(country = 'ru', callback) {
-            const result = http.get(`${apiUrl}`, 'country=ru&language=ru');
-            result.then(callback);
-        },
-        everything(query = '', callback) {},
-    }
-})();
-
-newsService.topHeadlines('', res => {
+newsService.topHeadlines('ru', res => {
     console.log(res);
 });
 
